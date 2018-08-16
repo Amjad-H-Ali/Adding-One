6ae6b88faacbd7fcbeff39d82f222002c4f2bd20
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-15 19:43:37 -0500

    We use recursion to carry the one if the digit was a 9

--- JS/index.js
+++ JS/index.js
@@ -2,16 +2,18 @@ console.log("Adding One!");
 
 const addOne = (array, indx) => {
 	// Set i equal to last index in array by default so user wont have to pass it in manually
-	const i = indx === undefined ? array.length - 1 : indx;
+	let i = indx === undefined ? array.length - 1 : indx;
 
+	// Just increment the digit in the array if it is less than 9 and we are done.
 	if (array[i] < 9) {
 		array[i] ++;
 		return array;
 	}
 	
+	// Otherwise, set it equal to 0 and carry the 1 using recursion
 	array[i] = 0;
-
 	i --;
+	return addOne(array, i);
 };
 
-console.log(addOne([2,3,1,2,8,9]));
+console.log(addOne([2,3,1,2,8,9])); //231290
