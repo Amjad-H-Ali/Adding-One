ef3f146a7fb85235663b6381a1e42e33a17c21cc
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-15 19:32:27 -0500

    Function adds one to last digit in array if it is less than 9

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,15 @@
 console.log("Adding One!");
 
 const addOne = (array, indx) => {
+	// Set i equal to last index in array by default so user wont have to pass it in manually
 	const i = indx === undefined ? array.length - 1 : indx;
 
+	if (array[i] < 9) {
+		array[i] ++;
+	}
+	return array;
+
+
 };
+
+console.log(addOne([2,3,1,2,8,6]));
