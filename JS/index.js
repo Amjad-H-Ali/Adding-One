79cd0fb090f67498131352edb2aa8b1d97fe35fa
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-15 20:00:15 -0500

    Added exit code if all nums were 9s; Therefore, we have to insert one at the first index and shift all elems to the right

--- JS/index.js
+++ JS/index.js
@@ -3,17 +3,31 @@ console.log("Adding One!");
 const addOne = (array, indx) => {
 	// Set i equal to last index in array by default so user wont have to pass it in manually
 	let i = indx === undefined ? array.length - 1 : indx;
-
+	// If we are beyond the first digit in the array, that means the first digit was a 9 that we set equal to 0
+	// So now all we have to do is shif every digit in the array to the left
+	// And set the first index to 1
+	if (i < 0) {
+		array.unshift(1);
+		return array;
+	};
 	// Just increment the digit in the array if it is less than 9 and we are done.
 	if (array[i] < 9) {
 		array[i] ++;
 		return array;
-	}
+	};
+
 	
 	// Otherwise, set it equal to 0 and carry the 1 using recursion
 	array[i] = 0;
 	i --;
+
 	return addOne(array, i);
 };
 
-console.log(addOne([2,3,1,2,8,9])); //231290
+console.log(addOne([1, 2, 3, 4])); //1,235
+
+console.log(addOne([9, 9, 9, 9])); //10,000
+
+console.log(addOne([1, 2, 9, 9])); //1,300
+
+console.log(addOne([0])); //1
